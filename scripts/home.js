f49228029bfeac879c03fd1835ce67a2d4efd907
ckmah <clarence.iyan@gmail.com>
2014-10-20 12:24:54 -0700

    cleaned up old timeline code, added preliminary twitter rss parser

--- scripts/home.js
+++ scripts/home.js
@@ -1,44 +1,52 @@
 /******************** HOMEPAGE SCRIPTS ********************/
 var numTest = 1;
+var tweets = [];
 
 function scrollTestimonials() {
 
- $testimonials = $('.testimonials');
+    $testimonials = $('.testimonials');
 
- setInterval(function() {
-  fadeOut($('#test' + numTest)); // fade out old
-  numTest = (numTest % 7) + 1; // increment
+    setInterval(function() {
+        fadeOut($('#test' + numTest)); // fade out old
+        numTest = (numTest % 7) + 1; // increment
 
-  setTimeout(function() {
-   fadeIn($('#test' + numTest)); // fade in new
-  }, 250);
- }, 5000);
+        setTimeout(function() {
+            fadeIn($('#test' + numTest)); // fade in new
+        }, 250);
+    }, 5000);
 }
 
 function initTestimonials() {
- $testimonials = $('.testimonials');
- // hide all tabs
- $testimonials.css('display', 'none');
+    $testimonials = $('.testimonials');
+    // hide all tabs
+    $testimonials.css('display', 'none');
 
- // only show tab 1
- $('#test1').removeAttr('style');
+    // only show tab 1
+    $('#test1').removeAttr('style');
 }
 
 function parseTwitterRSS() {
- $.get('https://script.google.com/macros/s/AKfycbzu7bVSXrtsFL63OFkPPH5XwI3V8XzOm6JAiNf6zr1gac8y4Xg/exec?520445386310164480', function(data) {
-  $(data).find("entry").each(function() { // or "item" or whatever suits your feed
-   var el = $(this);
+    console.log("Parsing Twitter Feed...");
+    $.get('https://script.google.com/macros/s/AKfycbzu7bVSXrtsFL63OFkPPH5XwI3V8XzOm6JAiNf6zr1gac8y4Xg/exec?520445386310164480', function(data) {
+        // find each item (tweet)
+        $(data).find("item").each(function() {
 
-   console.log("------------------------");
-   console.log("title      : " + el.find("title").text());
-   console.log("author     : " + el.find("author").text());
-   console.log("description: " + el.find("description").text());
-  });
- });
+            // limit to past 20 tweets
+            if (tweets.length > 20)
+                return;
+
+            // reference to found item (tweet)
+            var el = $(this);
+            var title = el.find("title").text();
+
+            // save tweet text
+            tweets[tweets.length] = title;
+        });
+    });
 }
 
 $(document).ready(function() {
- initTestimonials();
- scrollTestimonials();
- parseTwitterRSS();
+    initTestimonials();
+    scrollTestimonials();
+    parseTwitterRSS();
 });
